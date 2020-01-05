0a3d75381d3a6ef1c44d417c891fd3f24c0629a0
sweezy793 <sarthaks793@gmail.com>
2020-01-05 23:47:13 +0530

    Some styling

--- git-search/src/App.js
+++ git-search/src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import Navbar from './components/layout/Navbar'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className='App'>
-        <h1>Hola</h1>
+        <Navbar title="Git Search" />
       </div>
     );
   }
