400f5603d12001ef1ad2e0ef32772bf4a1daf985
sweezy793 <sarthaks793@gmail.com>
2020-01-06 00:32:45 +0530

    UserItem component

--- git-search/src/components/layout/Navbar.js
+++ git-search/src/components/layout/Navbar.js
@@ -1,27 +1,26 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
+class Navbar extends Component {
+  static defaultProps = {
+    title: 'Git Search',
+    icon: 'fab fa-github'
+  };
 
-export class Navbar extends Component {
-    static defaultProps = {
-        title: "Git Search",
-        icon: "fab fa-github"
-    }
+  static propTypes = {
+    title: PropTypes.string.isRequired,
+    icon: PropTypes.string.isRequired
+  };
 
-    static propTypes = {
-        title: PropTypes.string.isRequired,
-        icon: PropTypes.string.isRequired
-    }
-
-    render() {
-        return (
-            <nav className='navbar bg-primary'>
-                <h1>
-                    <i className={this.props.icon} /> {this.props.title}
-                </h1>
-            </nav>
-        )
-    }
+  render() {
+    return (
+      <nav className='navbar bg-primary'>
+        <h1>
+          <i className={this.props.icon} /> {this.props.title}
+        </h1>
+      </nav>
+    );
+  }
 }
 
-export default Navbar
+export default Navbar;
