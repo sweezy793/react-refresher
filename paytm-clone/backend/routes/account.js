85ae368e80224ef3658dfee843e3f10074c0481d
sweezy793 <sarthaks793@gmail.com>
2024-03-02 21:40:41 +0530

    backend completed

--- paytm-clone/backend/routes/account.js
+++ paytm-clone/backend/routes/account.js
@@ -1,6 +1,9 @@
 const express = require("express");
+const mongoose = require("mongoose");
+
 const authMiddleware = require("../middleware");
 const { Account } = require("../db");
+
 const router = express.Router();
 
 router.get("/balance", authMiddleware, async (req, res) => {
@@ -8,6 +11,56 @@ router.get("/balance", authMiddleware, async (req, res) => {
 
   return res.status(200).json({
     balance,
+  });
+});
+
+router.post("/transfer", authMiddleware, async (req, res) => {
+  const { amount, to } = req.body;
+
+  const account = await Account.findOne({
+    userId: req.userId,
+  });
+
+  if (account.balance < amount) {
+    return res.status(400).json({
+      message: "Insufficient balance",
+    });
+  }
+
+  const toAccount = await Account.findOne({
+    userId: to,
+  });
+
+  if (!toAccount) {
+    return res.status(400).json({
+      message: "Invalid account",
+    });
+  }
+
+  await Account.updateOne(
+    {
+      userId: req.userId,
+    },
+    {
+      $inc: {
+        balance: -amount,
+      },
+    }
+  );
+
+  await Account.updateOne(
+    {
+      userId: to,
+    },
+    {
+      $inc: {
+        balance: amount,
+      },
+    }
+  );
+
+  res.json({
+    message: "Transfer successful",
   });
 });
 
